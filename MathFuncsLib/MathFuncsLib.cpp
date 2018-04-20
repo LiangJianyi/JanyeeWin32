45ef4237c0c84b4f2e72ef7c0ce0dad97152539c
梁建溢 <liangjianyi.com@outlook.com>
2018-04-20 06:21:44 -0400

    Changesets-07

--- MathFuncsLib/MathFuncsLib.cpp
+++ MathFuncsLib/MathFuncsLib.cpp
@@ -25,3 +25,43 @@ namespace MathFuncs {
 		return a / b;
 	}
 }
+
+namespace JanyeeWin32Sundry {
+	std::string DateTime::DisplayCurrentTime() {
+		char buffer[64];
+		errNum = asctime_s(buffer, 64, &newtime);
+		if (errNum) {
+			return std::string("Error code: ").append(std::to_string(errNum));
+		}
+		else {
+			return buffer;
+		}
+	}
+	int DateTime::GetHour() {
+		return newtime.tm_hour;
+	}
+	int DateTime::IsDaylightSavingTime() {
+		return newtime.tm_isdst;
+	}
+	int DateTime::GetMonthOfDay() {
+		return newtime.tm_mday;
+	}
+	int DateTime::GetMinute() {
+		return newtime.tm_min;
+	}
+	int DateTime::GetMonth() {
+		return newtime.tm_hour;
+	}
+	int DateTime::GetSecond() {
+		return newtime.tm_sec;
+	}
+	int DateTime::GetWeekOfDay() {
+		return newtime.tm_wday;
+	}
+	int DateTime::GetYearOfDay() {
+		return newtime.tm_yday;
+	}
+	int DateTime::GetYear() {
+		return newtime.tm_year;
+	}
+}
--- MathFuncsLib/MathFuncsLib.h
+++ MathFuncsLib/MathFuncsLib.h
@@ -1,4 +1,8 @@
 #pragma once
+extern "C" {
+#include <time.h>
+}
+#include <string>
 // MathFuncsLib.h
 
 namespace MathFuncs {
@@ -15,5 +19,30 @@ namespace MathFuncs {
 
 		// Returns a / b
 		static double Divide(double a, double b);
+	};
+}
+
+namespace JanyeeWin32Sundry {
+	extern "C" static tm newtime;
+	extern "C" static errno_t errNum;
+	class DateTime {
+	private:
+		static time_t aclock;
+	public:
+		DateTime() {
+			// Get the number of seconds elapsed since midnight (00:00:00), January 1, 1970, UTC, according to the system clock. 
+			time(&aclock);
+			_localtime64_s(&newtime, &aclock);   // Convert time to struct tm form.  
+		}
+		static std::string DisplayCurrentTime();
+		static int GetHour();
+		static int IsDaylightSavingTime();
+		static int GetMonthOfDay();
+		static int GetMinute();
+		static int GetMonth();
+		static int GetSecond();
+		static int GetWeekOfDay();
+		static int GetYearOfDay();
+		static int GetYear();
 	};
 }
